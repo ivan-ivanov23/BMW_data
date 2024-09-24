21e0f5fee0c18ec995035f75faf62ce998e359c7
Ivan <iai5555@abv.bg>
2024-09-24 17:48:38 +0300

    fix clear button

--- window.py
+++ window.py
@@ -62,15 +62,11 @@ class MainWindow(QMainWindow):
         # Combo box for year
         self.year_box = QComboBox()
         self.year_box.setFixedSize(100, 20)
-        # Set default value as empty
-        self.year_box.setCurrentIndex(-1)
         self.year_box.setEnabled(False)
 
         # Combo box for modification
         self.mod_box = QComboBox()
         self.mod_box.setFixedSize(100, 20)
-        # Set default value as empty
-        self.mod_box.setCurrentIndex(-1)
         self.mod_box.setEnabled(False)
 
         self.button = QPushButton("Show Data")
@@ -105,10 +101,10 @@ class MainWindow(QMainWindow):
 
         # Connect the button to a function
         self.button.clicked.connect(self.on_button_click)
+        self.clear_button.clicked.connect(self.clear)
         self.gen_box.currentIndexChanged.connect(self.on_gen_change)
         self.year_box.currentIndexChanged.connect(self.on_year_change)
         self.mod_box.currentIndexChanged.connect(self.on_mod_change)
-        self.clear_button.clicked.connect(self.clear)
 
     def on_button_click(self):
         # Get the selected model from the combobox and display its name
@@ -185,27 +181,34 @@ class MainWindow(QMainWindow):
 
     def on_year_change(self):
         if self.year_box.currentText() != "":
+            # If the gen box is not empty, get the current generation
             # Get the current generation 
-            generation = self.gen_box.currentText()
-            # For current year, get all the modifications from the database and add them to the mod_box
-            # Once a year is selected, enable the mod_box
-            # But if the year is not selected, disable the mod_box
-            # Connect to database
-            basedir = os.path.abspath(os.path.dirname(__file__))
-            conn = sqlite3.connect(os.path.join(basedir, 'bmw_data.db'))
-            # Get a list of the values in the db
-            # Source: https://stackoverflow.com/questions/2854011/get-a-list-of-field-values-from-pythons-sqlite3-not-tuples-representing-rows
-            conn.row_factory = lambda cursor, row: row[0]
-            cursor = conn.cursor()
-            # Select all modifications for the current year
-            cursor.execute("SELECT Modification FROM " + generation + " WHERE Production = ?", (self.year_box.currentText(),))
-            # Make the modifications a set, so that they do not repeat
-            mods = set(cursor.fetchall())
-            # Add these modifications to the mod combo box
-            for i in mods:
-                self.mod_box.addItem(i)
-            self.mod_box.setCurrentIndex(-1)
-            self.mod_box.setEnabled(True)
+            if self.gen_box.currentText() == "E46" or self.gen_box.currentText() == "E90":
+                generation = self.gen_box.currentText()
+                # For current year, get all the modifications from the database and add them to the mod_box
+                # Once a year is selected, enable the mod_box
+                # But if the year is not selected, disable the mod_box
+                # Connect to database
+                basedir = os.path.abspath(os.path.dirname(__file__))
+                conn = sqlite3.connect(os.path.join(basedir, 'bmw_data.db'))
+                # Get a list of the values in the db
+                # Source: https://stackoverflow.com/questions/2854011/get-a-list-of-field-values-from-pythons-sqlite3-not-tuples-representing-rows
+                conn.row_factory = lambda cursor, row: row[0]
+                cursor = conn.cursor()
+                # Select all modifications for the current year
+                cursor.execute("SELECT Modification FROM " + generation + " WHERE Production = ?", (self.year_box.currentText(),))
+                # Make the modifications a set, so that they do not repeat
+                mods = set(cursor.fetchall())
+                # Add these modifications to the mod combo box
+                for i in mods:
+                    self.mod_box.addItem(i)
+                self.mod_box.setCurrentIndex(-1)
+                self.mod_box.setEnabled(True)
+            # Else return nothing so that clear works properly
+            # When the combo boxes are cleared in the clear method, the year_box and mod_box combo boxes are reset, 
+            # which may trigger the on_year_change method.
+            else:
+                return
         else:
             self.mod_box.clear()
             self.mod_box.setEnabled(False)
@@ -217,8 +220,6 @@ class MainWindow(QMainWindow):
             self.button.setEnabled(True)
         else:
             self.button.setEnabled(False)
-
-
 
     # Source: https://zetcode.com/pyqt/qwebengineview/
     def loadPage(self):
